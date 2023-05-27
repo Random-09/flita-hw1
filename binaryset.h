d7de3dc418be6926824d11bb408efafc1d2d0d0c
kvasa <kvasan2104@gmail.com>
2023-05-27 20:17:37 +0300

    changed header file

--- binaryset.h
+++ binaryset.h
@@ -12,25 +12,23 @@
 
 typedef struct binaryNode {
     char data[DATA_SIZE];
-    struct binaryNode *pointer;
+    struct binaryNode* pointer;
 } BinaryNode_t;
 
-BinaryNode_t *createBinaryNode(char *data);
+BinaryNode_t* createBinaryNode(char* data);
 
-void binarySetInput(BinaryNode_t **binaryHeadNode);
+void binarySetInput(BinaryNode_t** binaryHeadNode);
 
-void deleteBinarySet(BinaryNode_t *head);
+void deleteBinarySet(BinaryNode_t* head);
 
-void printBinarySet(BinaryNode_t *head);
+void printBinarySet(BinaryNode_t* head);
 
-bool isUniqueNode(BinaryNode_t *head, char *data);
+bool isUniqueNode(BinaryNode_t* head, char* data);
 
 bool isBinary(char data[]);
 
-bool isInt(char setLenStr[DATA_SIZE]);
+void printDecimalSet(BinaryNode_t* binaryHeadNode);
 
-void printDecimalSet(BinaryNode_t *binaryHeadNode);
-
-int binaryToDec(char *data);
+int binaryToDec(char* data);
 
 #endif
